0d2c644c491fed69aa6553437bcf9c8cc70f06af
ktk10191 <kyleknott11111@gmail.com>
2014-12-04 14:24:27 -0800

    commit before editing

--- server.js
+++ server.js
@@ -1,120 +1,48 @@
-// Server file that will be ran with 'node server.js' or 'nodemon server.js' if you want to have the ability to restart your server on the fly without stopping your server
+// server.js
 
-// Setting up the variables ======
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 
-    // Adding the variable 'express' to make sure that you require express since that is what is being used
-    var express = require('express');
-
-    // Firing the express framework
-    var app = express();
-
-    // Making sure we can use MongoDB in this app by requiring mongoose
-    var mongoose = require('mongoose');
-
-    // Morgan is being used to log requests to the console
-    var morgan = require('morgan');
-
-    // Body parser is pulling information from the HTML POST
-    var bodyParser = require('body-parser');
-
-    // DELETE and PUT simulation is what method-override does for you
-    var methodOverride = require('method-override');
-
-// config ======
-
-    // Connecting to a mongo database that is local. This would have to change if you were doing this on something like heroku
+// configuration ===========================================
     
-    mongoose.connect('mongodb://localhost/express-node')
+// config files
+var db = require('./config/db');
 
-    // Setting static file location from /public/something to /something for users. Just something to simplify things
-    app.use(express.static(__dirname + '/public'));
+// set our port
+var port = process.env.PORT || 8080; 
 
-    // log all requests to the console
-    app.use(morgan('dev'));
+// connect to our mongoDB database 
+// (uncomment after you enter in your own credentials in config/db.js)
+// mongoose.connect(db.url); 
 
-    // parse application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({'extended':'true'}));
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+app.use(bodyParser.json()); 
 
-    // parse application/vnd.api+json as json
-    app.use(bodyParser.json());
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
-    app.use(methodOverride());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-    
-    // Defining a model for mongoose ============
-    var Todo = mongoose.model('Todo', {
-      text : String
-    });
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
-    // Adding in routes ====================
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
 
-        // api routes getting back all of the todos that are being made
-        app.get('/api/todos', function(req, res) {
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
 
-          // Using mongoose to get everything from the database in the /api/todos path
-          Todo.find(function(err, todos) {
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);               
 
-            // if there is an error then it will show the error and then nothing after it will execute
-            if (err)
-              res.send(err)
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
 
-            res.json(todos); // Returning all of the todos in JSON format
-          });
-        });
-
-        // Return all Todos after created. This is also creating a todo with app.post
-        app.post('/api/todos', function(req, res) {
-          // creating a todo and the information is coming from an AJAX request from the Angular side
-          Todo.create({
-            text: req.body.text,
-            done: false
-          }, function(err, todo) {
-            if (err)
-              res.send(err);
-
-            // Return all todos after one is created
-            Todo.find(function(err, todos) {
-              if (err)
-                res.send(err)
-              res.json(todos);
-            });
-        });
-      });
-
-        // Deleting a todo
-
-        app.delete('/api/todos/:todo_id', function(req, res) {
-          Todo.remove({
-            _id : req.params.todo_id
-          }, function(err, todo) {
-            if (err)
-              res.send(err)
-
-            // Return the todos after deleting one
-            Todo.find(function(err, todos) {
-              if (err)
-                res.send(err)
-              res.json(todos);
-            });
-        });
-      });
-
-        // Application route ============
-
-        app.get('*', function(req, res) {
-          res.sendfile('./public/index.html'); // This is loading the single view page. Angular will handle all of the page changes on the front end
-        });
-
-    // Below is the code for telling node what port to listen on =============
-
-    app.listen(8080);
-    console.log("App listening on port 8080");
-
-
-
-
-
-
-
-
-
+// expose app           
+exports = module.exports = app;   
