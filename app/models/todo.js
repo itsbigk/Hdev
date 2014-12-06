5b5ce85a48144a8c1f78b1586fc8f73b5f61571c
ktk10191 <kyleknott11111@gmail.com>
2014-12-05 16:32:35 -0800

    added code to the app.js file and fixed some routing

---
+++ app/models/todo.js
@@ -0,0 +1,14 @@
+// need to require mongoose everyewhere that you want to save to your database
+// to avoid something like this then the file system would need to be entered into the serverjs file
+var mongoose = require('mongoose');
+
+// Adding a schema may be needed in the future for bigger and more complex databases
+// var TodoSchema = new mongoose.Schema({
+//   text: String
+// });
+
+// If using a schema then the schema name would be added to the mongoose model name when creating it so it knows what key and value pairs to have
+// Below is almost like adding a schema directly to the model itself
+var Todo = mongoose.model('Todo', {
+  text : String
+});
--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 var express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
-      // adding the bear model to the server
+      // logging requests
+      morgan = require('morgan'),
+      // adding the todo model to the server
       // this would almost eliminate the need for requiring specific folders in the filesystem
-      Bear = require('./app/models/bear'),
+      Todo = require('./app/models/todo'),
       // mongodb connection using mongoose
       mongoose = require('mongoose'),
-      // using the express router to make custom routes
-      router = express.Router(),
+      // setting the express router for defining custom routes for the api
+      router = express.Router();
       // setting the port that you want to use
       port = process.env.PORT || 8080;
 
@@ -16,6 +18,10 @@ mongoose.connect('mongodb://localhost/express-node');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(methodOverride());
+
+// rendering everything in the public folder. i can see this being good to use for single page apps
+app.use(express.static(__dirname + '/public'));
 
 
 // API routes
@@ -30,83 +36,84 @@ router.use(function(req, res, next) {
 
 // more routes for CRUD here
 
-// here it is for all routes that end in bears specifically
-// the bears api endpoint is http://localhost:8080/api/bears
-router.route('/bears')
-  // .post is indicating that this will be saving something to the server
-  .post(function(req, res) {
-    var bear = new Bear(); //creating a new instance of the bear model
-    bear.name = req.body.name; //setting the bears name depending on what the request says
-
-    // saving the bear and checking for errors
-    bear.save(function(err) {
-      if (err)
-       res.send(err);
-
-      res.json({ message: 'bear has been created!' });
-    });
-  })
+// here it is for all routes that end in todos specifically
+// the todos api endpoint is http://localhost:8080/api/todos
+router.route('/api/todos')
   // with chaining the methods in this cleaner way it is important to only put a semicolon on the end of the final method for the route
   .get(function(req, res) {
-    Bear.find(function(err, bears) {
+    Todo.find(function(err, todos) {
       if (err)
         res.send(err);
-      res.json(bears);
+      res.json(todos);
+    });
+  })
+  // .post is indicating that this will be saving something to the server
+  .post(function(req, res) {
+    // creating the todo and checking for errors
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // if there is no error then return all of the todos after another is created to update the list
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+  });
+  
+  // deleting and updating a todo is all that is really needed for this section unless there is a need to look up one specific todo in the future
+  // if that is the case then all that would be needed to be added is a get method 
+  router.route('/api/todos/:todo_id')
+
+    // deleting a todo with a specific id
+    .delete(function(req, res) {
+        Todo.remove({
+          _id : req.params.todo_id
+        }, function(err, todo) {
+          if (err)
+          res.send(err)
+        // return all of the todo items after one is deleted with no errors
+        Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+        res.json(todos);
+      });
     });
   });
 
-  // getting a specific bear from the databse back
-  router.route('/bears/:bear_id')
-
-    // making a get method that will take a bear from the database with a specific id and return the json data for that object
-    .get(function(req, res) {
-      Bear.findById(req.params.bear_id, function(err, bear) {
-        if (err)
-          res.send(err)
-        res.json(bear);
-      });
-    })
-
-    // updating a specific bear by id
+    // updating a specific todo by id
     .put(function(req, res) {
-      // once again using the bear model to find a specific bear by id
-      Bear.findById(req.params.bear_id, function(err, bear) {
+      // once again using the todo model to find a specific todo by id
+      Todo.findById(req.params.todo_id, function(err, todo) {
         if (err)
           res.send(err);
 
-        bear.name = req.body.name; //updating the bear's info here
+        todo.text = req.body.text; //updating the todo's info here
 
-        // saving the bear after the update has been completed
-        bear.save(function(err) {
+        // saving the todo after the update has been completed
+        todo.save(function(err) {
           if (err) 
             res.send(err);
 
-          res.json({ message: 'bear has been updated!' });
+          res.json({ message: 'todo has been updated!' });
         });
       });
     })
-
-    // deleting a bear with a specific id
-    .delete(function(req, res) {
-      Bear.remove({
-        _id: req.params.bear_id
-      }, function(err, bear){
-          if (err)
-            res.send(err)
-
-          res.json({ message: 'successfully deleted' });
-      });
-    });
-
-
 
   // all of the routes are currently set up to be used as api routes
   // this line creates a new path that the client can access called /api so it would look like http://localhost:8080/api
   app.use('/api', router);
 
 // testing the route to make sure everything is connected
-router.get('/', function(req, res) {
-  res.json({ message: 'everything is working for this api route' });
+router.get('*', function(req, res) {
+  // only need one route for everything because angular will be handling all of the frontend work
+  res.sendfile('./public/index.html')
+  console.log('welcome to the homepage');
 });
 
 // run server
