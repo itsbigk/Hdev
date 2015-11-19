2499fa88917a564c2afaead147e8d5efa7aaab79
Kyle Knott <kyle.knott@deutschinc.com>
2015-11-19 00:11:27 -0800

    begin react and flux

--- public/bundle.js
+++ public/bundle.js
@@ -24850,46 +24850,9 @@
 	  value: true
 	});
 
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
 	var _flux = __webpack_require__(213);
 
-	var DispatcherClass = (function (_Dispatcher) {
-	  _inherits(DispatcherClass, _Dispatcher);
-
-	  function DispatcherClass() {
-	    _classCallCheck(this, DispatcherClass);
-
-	    _get(Object.getPrototypeOf(DispatcherClass.prototype), 'constructor', this).apply(this, arguments);
-	  }
-
-	  _createClass(DispatcherClass, [{
-	    key: 'handleAction',
-	    value: function handleAction(action) {
-	      console.log('In dispatcher for action: %s', action);
-	      if (!action.type) {
-	        throw new Error('Empty action');
-	      }
-
-	      this.dispatch({
-	        type: action.type,
-	        action: action
-	      });
-	    }
-	  }]);
-
-	  return DispatcherClass;
-	})(_flux.Dispatcher);
-
-	var AppDispatcher = new DispatcherClass();
-
-	exports['default'] = AppDispatcher;
+	exports['default'] = new _flux.Dispatcher();
 	module.exports = exports['default'];
 
 /***/ },
--- src/dispatchers/AppDispatcher.js
+++ src/dispatchers/AppDispatcher.js
@@ -1,20 +1,3 @@
 import { Dispatcher } from 'flux'
 
-class DispatcherClass extends Dispatcher {
-
-  handleAction(action) {
-    console.log('In dispatcher for action: %s', action)
-    if(!action.type) {
-      throw new Error('Empty action')
-    }
-
-    this.dispatch({
-      type: action.type,
-      action: action
-    })
-  }
-}
-
-const AppDispatcher = new DispatcherClass()
-
-export default AppDispatcher
+export default new Dispatcher()
