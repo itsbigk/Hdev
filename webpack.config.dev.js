787fd6677f3bcbbc6d834bcfc002f4733ca5dac0
Kyle Knott <kyle.knott@deutschinc.com>
2016-01-15 17:04:58 -0800

    update package and readme

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,8 +9,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        BROWSER: JSON.stringify(true) //,
-        // NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+        BROWSER: JSON.stringify(true)
       }
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
